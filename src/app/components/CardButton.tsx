836c4df95ab4150a6c163b3f3c65819eb8017372
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2023-10-15 15:28:38 +0200

    fix: minor button tweaks

--- src/app/components/CardButton.tsx
+++ src/app/components/CardButton.tsx
@@ -1,7 +1,7 @@
 const CardButton = ({ title = '' }) => {
   return (
-    <div className="flex h-full w-full flex-col justify-center">
-      <div className="translate-y-0 transform border-4 border-orange-500 p-6 transition-all duration-200 ease-in lg:relative lg:border-transparent lg:group-hover:border-orange-500">
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      <div className="w-2/3 translate-y-0 transform border-4 border-orange-500 py-6 transition-all duration-200 ease-in lg:relative lg:border-transparent lg:group-hover:border-orange-500">
         <h2 className="duration-400 flex translate-y-0 transform items-center justify-center text-2xl uppercase text-orange-500 transition-transform ease-in-out group-hover:text-orange-500 lg:absolute lg:inset-[56px] lg:text-white lg:group-hover:-translate-y-8">
           {title}
         </h2>
--- src/app/components/CategoryCard.tsx
+++ src/app/components/CategoryCard.tsx
@@ -9,7 +9,7 @@ const CategoryCard = ({
 }: CategoryCardProps) => {
   return (
     <Link href="/" className="group w-full">
-      <div className="lg:p-30 relative flex aspect-square min-w-[300px] items-center justify-center p-12 transition-all duration-300 ease-in-out lg:group-hover:-translate-y-4 lg:group-hover:shadow-2xl">
+      <div className="relative flex aspect-square min-w-[300px] items-center justify-center transition-all duration-300 ease-in-out lg:group-hover:-translate-y-4 lg:group-hover:shadow-2xl">
         <div className="absolute inset-0 overflow-hidden">
           <BackgroundImageLoader alt={categoryTitle} src={categoryImageUrl} />
         </div>
